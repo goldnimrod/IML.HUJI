eb6d87226f2458a34a4e3f5e1590f90202f4da77
Nimrod Gold <goldnimrod@gmail.com>
2022-03-27 08:14:02 +0300

    square loss

--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -16,10 +16,11 @@ def mean_square_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     -------
     MSE of given predictions
     """
-    raise NotImplementedError()
+    return np.linalg.norm(y_pred - y_true) ** 2
 
 
-def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: bool = True) -> float:
+def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray,
+                            normalize: bool = True) -> float:
     """
     Calculate misclassification loss
 
